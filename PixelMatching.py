73261cbc66b4ddaccc7864330180bcbe6b2a6584
SEbert171 <simon.ebert.kn@gmx.de>
2024-08-28 19:38:57 +0200

    Added Raster search based on rasterio

--- PixelMatching.py
+++ PixelMatching.py
@@ -1,17 +1,62 @@
 import geopandas as gpd
 import rasterio.mask
 import rasterio.plot
+import numpy as np
+import shapely
+import numpy.linalg as la
 
-def find_matching_area(raster_image, tracked_point: gpd.GeoDataFrame, matching_radius: int=5, search_radius: int=30):
-    tracked_point = tracked_point.to_crs(raster_image.crs)
 
-    if len(tracked_point)!=1:
+def get_buffer_around_point(raster_image, center_point: gpd.GeoDataFrame, buffer_radius: int = 30):
+    center_point = center_point.to_crs(raster_image.crs)
+
+    if len(center_point) != 1:
         raise ValueError("There is not exactly one point to be tracked.")
+
+    masking_shape_matching_area = center_point.buffer(distance=buffer_radius)
+    matching_area, matching_area_transform = rasterio.mask.mask(dataset=raster_image,
+                                                                shapes=masking_shape_matching_area, crop=True)
+
+    return matching_area, matching_area_transform
+
+
+def find_matching_area(tracked_image, search_image, tracked_point: gpd.GeoDataFrame, matching_radius: int = 5, search_radius: int = 10):
+    global optimal_match_point
+    tracked_point = tracked_point.to_crs(tracked_image.crs)
+
+    if len(tracked_point) != 1:
+        raise ValueError("There is not exactly one point to be tracked.")
+
+    if tracked_image.crs != search_image.crs:
+        raise ValueError("The two raster images are not based on the same crs")
 
     #if ~tracked_point.touches(raster_image):
         #raise ValueError("Tracked point and raster image do not intersect.")
 
-    masking_shape_matching_area = tracked_point.buffer(distance=matching_radius)
-    matching_area, matching_area_transform = rasterio.mask.mask(dataset=raster_image, shapes=masking_shape_matching_area, crop=True)
+    matching_area, matching_area_transform = get_buffer_around_point(raster_image=tracked_image,
+                                                                     center_point=tracked_point,
+                                                                     buffer_radius=matching_radius)
+
+    search_area, search_area_transform = get_buffer_around_point(raster_image=search_image,
+                                                                 center_point=tracked_point,
+                                                                 buffer_radius=search_radius)
+
     rasterio.plot.show(matching_area)
-    print(matching_area)
+    height = search_area.shape[1]
+    width = search_area.shape[2]
+    cols, rows = np.meshgrid(range(width), range(height))
+    x, y = rasterio.transform.xy(search_area_transform, rows, cols)
+    longitudes = np.array(x[0])
+    latitudes = np.array(y[0])
+    best_loss = np.inf
+    for lon in longitudes:
+        for lat in latitudes:
+            center_point = gpd.GeoDataFrame(index=[0], crs=tracked_image.crs, geometry=[shapely.Point(lon, lat)])
+            compare_area, compare_area_transform = get_buffer_around_point(raster_image=search_image,
+                                                                           center_point=center_point,
+                                                                           buffer_radius=matching_radius)
+            loss = la.norm(matching_area[0]-compare_area[0], ord=1)
+            if loss < best_loss:
+                best_loss = loss
+                optimal_match_point = [lon, lat]
+    return optimal_match_point
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from CreateGeometries.CreateGeometries import grid_points_on_polygon
 from dataloader import read_files
 from PixelMatching import find_matching_area
+from PixelMatching import get_buffer_around_point
 
 path1 = "../Test_Data/KBT_hillshade_2019-07.tif.tiff"
 path2 = "../Test_Data/KBT_hillshade_2021-08.tif.tiff"
@@ -20,4 +21,9 @@ polygon = polygon.to_crs(crs=3035)
 points = grid_points_on_polygon(polygon=polygon, number_of_points=100)
 
 
-find_matching_area(file1, points.iloc[[0]], matching_radius=2)
+#optimal_match = find_matching_area(file1, file2, points.iloc[[0]], matching_radius=2)
+
+trial_area1 = get_buffer_around_point(file1, points.iloc[[50]], 200)[0][0]
+trial_area2 = get_buffer_around_point(file2, points.iloc[[50]], 200)[0][0]
+print(trial_area1[1000:1018, 1000:1018])
+print(trial_area2[1000:1018, 1000:1018])
