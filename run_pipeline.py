4e1f5ee7e722a2c99c9b6718da849f3226aa2c44
SimonEbert <simon.ebert.kn@gmx.de>
2025-10-14 12:14:26 +0200

    Changed input folder structure to a relative path

--- run_pipeline.py
+++ run_pipeline.py
@@ -32,7 +32,7 @@ import json
 # ==============================
 # USER CONFIGURATION (paths, data names, CRS)
 # ==============================
-input_folder = "/media/simon/Swap/Dokumente/Studium/14.Semester/HiWi_Arbeit_PyImageTrack/Lisa_Kaunertal/Testdaten_Alignment"
+input_folder = "../Lisa_Kaunertal/Testdaten_Alignment"
 date_csv_path = os.path.join(input_folder, "image_dates.csv") # can be  set to = None if the day is reflected in the filename
 #date_csv_path = None
 pairs_csv_path = os.path.join(input_folder, "image_pairs.csv") # can be  set to = None if all or successive pairing mode is selected below
@@ -42,7 +42,7 @@ poly_outside_filename = "stable_area_drone.shp"
 poly_inside_filename  = "moving_area_drone.shp"
 poly_CRS = 32632
 
-output_folder = "/media/simon/Swap/Dokumente/Studium/14.Semester/HiWi_Arbeit_PyImageTrack/Lisa_Kaunertal/test_results"
+output_folder = "../Lisa_Kaunertal/test_results"
 pairing_mode = "custom"            # options: "all", "successive", "custom" (=from image_pairs.csv)
 
 use_fake_georeferencing = False        # set True only when processing non-ortho JPGs
