Resolved wrong clahe calculation for multi-channel images

src/PyImageTrack/DataProcessing/ImagePreprocessing.py
@@ -32,8 +32,12 @@ def equalize_adapthist_images(image_matrix, kernel_size, clip_limit):
     np.ndarray
         The contrast-enhanced image matrix.
     """
-    equalized_image = skimage.exposure.equalize_adapthist(image=image_matrix.astype(np.uint16), kernel_size=kernel_size,
+    # Skimage expects images in the format (x,y,c), i.e. channels in the last dimension
+    image_matrix_transposed = image_matrix.transpose((2,1,0))
+    equalized_image = skimage.exposure.equalize_adapthist(image=image_matrix_transposed.astype(np.uint16), kernel_size=kernel_size,
                                                           clip_limit=clip_limit)
+    # Retranspose to match the convention in the rest of the script
+    equalized_image = equalized_image.transpose((2,1,0))
     return equalized_image
 
 

src/PyImageTrack/ImageTracking/ImagePair.py
@@ -1438,7 +1438,7 @@ class ImagePair:
                 # --- Save input images if requested ---
 
         if "first_image_matrix_jpg" in save_files:
-            image1_matrix_uint = (self.image1_matrix * 255/self.image1_matrix.max())
+            image1_matrix_uint = (self.image1_matrix * self.image1_matrix_original.max()/self.image1_matrix.max())
 
             _save_raster_as_file(
                 path=f"{folder_path}/image_{self.image1_observation_date.strftime(format='%Y-%m-%d')}.jpeg",
@@ -1449,8 +1449,7 @@ class ImagePair:
             )
 
         if "second_image_matrix_jpg" in save_files:
-
-            image2_matrix_uint = (self.image2_matrix * 255/self.image2_matrix.max())
+            image2_matrix_uint = (self.image2_matrix * self.image2_matrix_original.max()/self.image2_matrix.max())
             _save_raster_as_file(
                 path=f"{folder_path}/image_{self.image2_observation_date.strftime(format='%Y-%m-%d')}.jpeg",
                 raster=image2_matrix_uint.astype(np.uint8),